b6e47267cb7fa083410acbe4c293a8355cadb4f8
Anuar Harb <anuarharb@hotmail.com>
2018-10-22 20:11:07 -0500

    radio buttons

---
+++ scripts.js
@@ -0,0 +1,16 @@
+
+
+
+
+
+
+const inputs = document.querySelectorAll('input[name=pet]');
+
+for(let i = 0; i < inputs.length; i++) {
+  inputs[i].addEventListener('click', choosePet);
+}
+
+function choosePet(event) {
+  let pet = event.target.value;
+  alert(pet);
+}
